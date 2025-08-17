850cddc3aa8858db43783ca7bdc6f954a8876c07
inss <fyzzy1943@gmail.com>
2025-08-17 22:45:41 +0800

    .

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -20,16 +20,21 @@ const breadcrumbs: BreadcrumbItem[] = [
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
+                    到底咋显示的啊卧槽Σ(°ロ°)
+                    <Link :href="route('login')">tst</Link>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
+                    突出一个复杂
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
+                    这块又是哪？
                 </div>
             </div>
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                 <PlaceholderPattern />
+                这里显示点啥？
             </div>
         </div>
     </AppLayout>
