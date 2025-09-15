51d6947f0fefd97e0a0ab569385f27b0ee62126b
inss <fyzzy1943@gmail.com>
2025-09-15 09:13:29 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\DallyController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\DallyController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -15,9 +14,8 @@ Route::get('dashboard', function () {
 
 Route::get('dally', [DallyController::class, 'index']);
 
-
-Route::get('dally/new', [DallyController::class, 'create'])->name('dally.create');
-// Route::group
-
+-Route::get('dally/new', [DallyController::class, 'create'])->name('dally.create');
+-// Route::group
+    
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
